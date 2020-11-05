fb01016a827dc55ca426c393e81cfb4191170baa
Tyler Hales <10869186@my.uvu.edu>
2020-11-04 21:10:38 -0700

    Random Placement Python

---
+++ Python Scripts/RandomPlacementDebugging.py
@@ -0,0 +1,38 @@
+import maya.cmds as cmds
+import random
+
+duplicates = 3
+
+# random.randrange(-20,20)
+
+objects = cmds.ls(sl=True)
+
+
+
+# copies.extend(objectCopies)
+
+for o in objects:
+
+    for i in range(0,duplicates):
+        
+        objectCopy = cmds.duplicate(o)
+        
+        print i
+        
+        cmds.move(random.randrange(-20,20), random.randrange(-20,20), random.randrange(-20,20),
+                    objectCopy,
+                    relative=1,
+                    objectSpace=1,
+                    worldSpaceDistance=1) 
+                    
+        #cmds.move((i+1)*-3, (i+1)*-3, (i+1)*-3,
+                    #objectCopy,
+                    #relative=1,
+                    #objectSpace=1,
+                    #worldSpaceDistance=1)                    
+                 
+        # break
+        
+        # print objectCopy
+    
+    # break
