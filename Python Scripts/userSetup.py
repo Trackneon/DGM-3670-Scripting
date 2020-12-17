1f12c289ae94a6157175fdca267860c9bd2ad82b
Tyler Hales <10869186@my.uvu.edu>
2020-12-17 10:05:27 -0700

    Updates

--- Python Scripts/userSetup.py
+++ Python Scripts/userSetup.py
@@ -4,5 +4,5 @@ import sys
 if not cmds.commandPort(':4434', query=True):
     cmds.commandPort(name=':4434')
 
-if 'D:/UVU/Fall 2020/DGM 3670/Git Repo/DGM 3670 Scripting/Python Scripts/my_tools' not in sys.path:
-    sys.path.append('D:/UVU/Fall 2020/DGM 3670/Git Repo/DGM 3670 Scripting/Python Scripts/my_tools')
+if 'D:/UVU/Fall 2020/DGM 3670/Git Repo/DGM 3670 Scripting/Python Scripts' not in sys.path:
+    sys.path.append('D:/UVU/Fall 2020/DGM 3670/Git Repo/DGM 3670 Scripting/Python Scripts')
